bcbbd526fcc9e0f3191f3e811ec5b6c1a7c14041
Ramu S <ramusimhadri691@gmail.com>
2026-05-07 19:16:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-from fastapi import FastAPI
+import streamlit as st
 import joblib
 import pandas as pd
 
-app = FastAPI()
-
-# Load trained model
+# Load model
 model = joblib.load("best_model.pkl")
 
-@app.get("/")
-def home():
+st.title("Forecast Prediction App")
 
-    return {
-        "message": "Forecast API Running"
-    }
+st.write("Enter input values:")
 
-@app.get("/forecast")
-def forecast():
+lag_1 = st.number_input("lag_1", value=200)
+lag_7 = st.number_input("lag_7", value=180)
+lag_30 = st.number_input("lag_30", value=150)
+rolling_mean_7 = st.number_input("rolling_mean_7", value=190)
+rolling_std_7 = st.number_input("rolling_std_7", value=12)
+day_of_week = st.number_input("day_of_week", value=2)
+month = st.number_input("month", value=5)
+holiday_flag = st.number_input("holiday_flag", value=0)
+
+if st.button("Predict"):
 
     sample_data = pd.DataFrame({
 
-        'lag_1': [200],
-        'lag_7': [180],
-        'lag_30': [150],
-        'rolling_mean_7': [190],
-        'rolling_std_7': [12],
-        'day_of_week': [2],
-        'month': [5],
-        'holiday_flag': [0]
+        'lag_1': [lag_1],
+        'lag_7': [lag_7],
+        'lag_30': [lag_30],
+        'rolling_mean_7': [rolling_mean_7],
+        'rolling_std_7': [rolling_std_7],
+        'day_of_week': [day_of_week],
+        'month': [month],
+        'holiday_flag': [holiday_flag]
 
     })
 
     prediction = model.predict(sample_data)
 
-    return {
-        "forecast": prediction.tolist()
-    }
+    st.success(f"Forecast Prediction: {prediction[0]}")

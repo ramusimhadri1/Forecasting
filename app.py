d5f9523ef74add6c0a3b4a8c395b94f33ede2895
Ramu S <ramusimhadri691@gmail.com>
2026-05-07 19:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-# Load model
 model = joblib.load("best_model.pkl")
 
 st.title("Forecast Prediction App")
-
-st.write("Enter input values")
 
 lag_1 = st.number_input("lag_1", value=200)
 lag_7 = st.number_input("lag_7", value=180)
@@ -21,16 +18,16 @@ holiday_flag = st.number_input("holiday_flag", value=0)
 if st.button("Predict"):
 
     sample_data = pd.DataFrame({
-        'lag_1': [lag_1],
-        'lag_7': [lag_7],
-        'lag_30': [lag_30],
-        'rolling_mean_7': [rolling_mean_7],
-        'rolling_std_7': [rolling_std_7],
-        'day_of_week': [day_of_week],
-        'month': [month],
-        'holiday_flag': [holiday_flag]
+        "lag_1": [lag_1],
+        "lag_7": [lag_7],
+        "lag_30": [lag_30],
+        "rolling_mean_7": [rolling_mean_7],
+        "rolling_std_7": [rolling_std_7],
+        "day_of_week": [day_of_week],
+        "month": [month],
+        "holiday_flag": [holiday_flag]
     })
 
     prediction = model.predict(sample_data)
 
-    st.success(f"Forecast Prediction: {prediction[0]}")
+    st.success(f"Forecast: {prediction[0]}")
